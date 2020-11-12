1fde84d5f3d5de320633d89b0a124f877b14ef4a
Silver Sabre <38342552+SliverSabre@users.noreply.github.com>
2020-11-12 15:53:33 +0800

    没必要添加的Demo

--- Demo.py
+++
@@ -1,9 +0,0 @@
-import sort_out as SOF
-Dirs=SOF.Sort_Out("/home/xxzx/Pictures")
-#Dirs.path="/home/xxzx" #If you want to change path,do this
-Dirs.sort_out_by_time("ctime","all")
-#Dirs.sort_out_by_key("")
-#Dirs.sort_out_by_filetype()
-#Dirs.path="/home/xxzx/Pictures/图片"
-#Dirs.sort_out_by_filetype()
-#Dirs.recover()
