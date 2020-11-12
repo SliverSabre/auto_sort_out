099c3a45a8498cc8565d11e9b10068ac818993df
Silver Sabre <38342552+SliverSabre@users.noreply.github.com>
2020-11-12 15:14:59 +0800

    Demo

--- Demo.py
+++ Demo.py
@@ -1,6 +1,9 @@
 import sort_out as SOF
-Dirs=SOF.file_sort("/home/xxx/Documents")
-#Dirs.path="/home/xxx/Music" #If you want to change path,do this
+Dirs=SOF.Sort_Out("/home/xxzx/Pictures")
+#Dirs.path="/home/xxzx" #If you want to change path,do this
 Dirs.sort_out_by_time("ctime","all")
-Dirs.sort_out_by_key("report")
-#Dirs.sort_out_by_filetype() #Is not completed.
+#Dirs.sort_out_by_key("")
+#Dirs.sort_out_by_filetype()
+#Dirs.path="/home/xxzx/Pictures/图片"
+#Dirs.sort_out_by_filetype()
+#Dirs.recover()
