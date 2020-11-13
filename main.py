df6f55f2bffa5a60a549a26429a6ed43da149abf
Silver Sabre <38342552+SliverSabre@users.noreply.github.com>
2020-11-13 13:59:11 +0800

    看上去靠谱的实例......

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
-import sort_out
-import Tkinter
+import sort_out as SOF
 
-#准备编写图形化程序
+path=input("Where the directory you want to sort out?")
+f=SOF.Sort_Out(str(path))
+cm=input("What kind of type do you want to use?")
+print("-----------------\n1:Sort out by file's time infomation.\n2:Sort out by file's type.\n3:Sort out by keywords.\n----------------------\nCommand 'q' to quit.")
+while true:
+  if cm == '1':
+    f.sort_out_by_time("mtime","normal")
+  elif cm == '2':
+    f.sort_out_by_filetype()
+  elif cm =='3':
+    Keys=input("Input your keywords:")
+    f.sort_out_by_key(Keys)
+  elif cm=='q':
+    break
+  else:
+    print("Error,you input an wrong command!")
+    continue
+f.close()
